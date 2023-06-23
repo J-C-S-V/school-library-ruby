3913ef3e61ac0ce82d4c04fa8bb6d5c3fdae3d1e
Juan Sanchez <juansan1993@hotmail.com>
2023-06-23 10:59:21 -0500

    Update relationships

--- book.rb
+++ book.rb
@@ -1,8 +1,13 @@
 class Book
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = [] # Create the has-many side (a book has many rentals).
+  end
+
+  def add_rental(rental)
+    @rentals << rental # The << operator is used to push an element into an array.
   end
 end
--- rental.rb
+++ rental.rb
@@ -6,7 +6,7 @@ class Rental
     @book = book
     @person = person
 
-    @book.rentals << self # Create the has-many side (a book has many rentals).
+    @book.add_rental(self) # Create the belongs-to side (a rental belongs to a book).
     @person.rentals << self # Create the has-many side (a person has many rentals).
   end
 end
