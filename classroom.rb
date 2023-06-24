a31a66b5f6afb05c4a8229934b10a00b76f6a34f
Juan Sanchez <juansan1993@hotmail.com>
2023-06-24 13:02:25 -0500

    Remove comments

--- classroom.rb
+++ classroom.rb
@@ -3,11 +3,10 @@ class Classroom
 
   def initialize(label)
     @label = label
-    @students = [] # Create the has-many side (a classroom has many students).
-  end
+    @students = []
 
   def add_student(student)
     @students << student
-    student.classroom = self # Create the belongs-to side (a student belongs to a classroom).
+    student.classroom = self
   end
 end
